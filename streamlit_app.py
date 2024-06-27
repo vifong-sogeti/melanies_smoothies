495c5a0f13b9e6c56c252c0b0c14f94f7618cf4d
vifong-sogeti <144886265+vifong-sogeti@users.noreply.github.com>
2024-06-27 11:26:11 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,27 +12,27 @@ st.write(
 name_on_order = st.text_input("Name on Smoothie")
 st.write("The name on your Smoothie will be:", name_on_order)
 
-# cnx = st.connected("snowflake")
-# session = cnx.session()
-# my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+cnx = st.connection("snowflake")
+session = cnx.session()
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 
-# ingredients_list = st.multiselect(
-#     'Chooseup to 5 ingredients'
-#     , my_dataframe
-#     , max_selections = 5
-#     )
+ingredients_list = st.multiselect(
+    'Chooseup to 5 ingredients'
+    , my_dataframe
+    , max_selections = 5
+    )
 
-# if ingredients_list:
-#     ingredients_string = ''
+if ingredients_list:
+    ingredients_string = ''
     
-#     for fruit_chosen in ingredients_list:
-#         ingredients_string += fruit_chosen + ' '
+    for fruit_chosen in ingredients_list:
+        ingredients_string += fruit_chosen + ' '
 
-#     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
-#             values ('""" + ingredients_string + """', '""" + name_on_order + """')"""
+    my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
+            values ('""" + ingredients_string + """', '""" + name_on_order + """')"""
 
-#     time_to_insert = st.button('Submit Order')
+    time_to_insert = st.button('Submit Order')
     
-#     if time_to_insert:
-#         session.sql(my_insert_stmt).collect()
-#         st.success('Your Smoothie is ordered!', icon="✅")
+    if time_to_insert:
+        session.sql(my_insert_stmt).collect()
+        st.success('Your Smoothie is ordered!', icon="✅")
