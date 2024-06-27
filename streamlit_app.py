c82d92b0edd01ca3520b00de39c77e59eb29282b
vifong-sogeti <144886265+vifong-sogeti@users.noreply.github.com>
2024-06-27 12:03:55 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,9 +16,9 @@ st.write("The name on your Smoothie will be:", name_on_order)
 
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('SEARCH_ON'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
 st.dataframe(data=my_dataframe, use_container=True)
-st.stop
+st.stop()
 
 ingredients_list = st.multiselect(
     'Chooseup to 5 ingredients'
