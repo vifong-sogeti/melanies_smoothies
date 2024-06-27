b7ad751d29c80d2730e30bf5311175b610a2baed
vifong-sogeti <144886265+vifong-sogeti@users.noreply.github.com>
2024-06-27 11:41:08 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,6 @@ import streamlit as st
 from snowflake.snowpark.functions import col
 import requests
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothi :cup_with_straw:")
@@ -25,14 +24,13 @@ ingredients_list = st.multiselect(
     , max_selections = 5
     )
 
-st.text(fruityvice_response)
-fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
-
 if ingredients_list:
     ingredients_string = ''
     
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
             values ('""" + ingredients_string + """', '""" + name_on_order + """')"""
